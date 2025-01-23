4a662b3ceb5e00db9ecedabd7d2fdff050aac556
Samson Okunola <smsnmichea@gmail.com>
2025-01-23 20:55:06 +0100

    chore: added user store

---
+++ src/store/chunks/user.ts
@@ -0,0 +1,47 @@
+import Cookies from "js-cookie";
+
+import { createPersistMiddleware } from "../middleware";
+import type { Maybe, UserProps } from "@/types";
+
+interface SignOutOptions {
+	redirectUrl?: string;
+	soft?: boolean;
+}
+
+interface UserStore {
+	user: Maybe<UserProps>;
+	signin: (user: UserProps) => void;
+	signout: (options?: SignOutOptions) => void;
+}
+
+const initialState: UserStore = {
+	user: null,
+	signin: () => {},
+	signout: () => {},
+};
+
+const useUserStore = createPersistMiddleware<UserStore>("classore-marketers", (set) => ({
+	...initialState,
+	signin: (user) => {
+		set({ user });
+		Cookies.set("classore-marketer", "", {
+			expires: 1000 * 60 * 60 * 24 * 7,
+			sameSite: "strict",
+		});
+	},
+	signout: (options) => {
+		try {
+			if (!options?.soft) {
+				set({ user: null });
+			}
+			Cookies.remove("classore-marketer");
+		} catch (error) {
+			console.error(error);
+		} finally {
+			window.location.href = options?.redirectUrl || "/";
+			window.localStorage.removeItem("classore-marketers");
+		}
+	},
+}));
+
+export { useUserStore };
