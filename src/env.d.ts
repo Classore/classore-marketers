95a7b96d925b00fc4b2c4ed9361826c5b11b97be
Samson Okunola <smsnmichea@gmail.com>
2025-05-25 19:46:08 +0100

    chore: updates

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 export const requiredEnvs = [
 	"API_URL",
 	"NEXT_PUBLIC_API_URL",
+	"NEXT_PUBLIC_NODE_ENV",
 	"NODE_ENV",
 	"TESTING",
 ] as const;
@@ -12,6 +13,7 @@ declare global {
 		interface ProcessEnv extends Record<RequiredEnvs, string> {
 			readonly API_URL: string;
 			readonly NEXT_PUBLIC_API_URL: string;
+			readonly NEXT_PUBLIC_NODE_ENV: "development" | "production";
 			readonly NODE_ENV: "development" | "production" | "test";
 			readonly TESTING: string;
 		}
